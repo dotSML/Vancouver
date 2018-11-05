247f5e468b508dc8042f47d5f90f95dd8b57e956
Sten-Mark Laur <sten@sml.ee>
2018-11-05 14:13:02 +0200

    Code cleanup

---
+++ wwwroot/js/activeItems.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var pathName = window.location.pathname;
+    console.log($('#aboutActive'));
+
+    if (pathName === "/about") {
+    }
+
+    switch (pathName) {
+    case '/about':
+        $(document).remove('active');
+        $('#aboutNav').addClass('active');
+        break;
+    case '/index':
+        $(document).remove('active');
+        $('#indexNav').addClass('active');
+        break;
+    case '/tickettest':
+        $(document).remove('active');
+        $('#myTicketsNav').addClass('active');
+        break;
+    case '/MyAccount':
+        $(document).remove('active');
+        $('#myAccountNav').addClass('active');
+        break;
+    }
+});
---
+++ wwwroot/js/ajaxLogin.js
@@ -0,0 +1,34 @@
+﻿$('#loginSubmit').on('click', function () {
+    var userEmail = $('#signin-email').val();
+    var userPassword = $('#signin-password').val();
+    var userRemember = $('#remember-me').val();
+    var userObject = new Object();
+    userObject.InputEmail = userEmail;
+    userObject.InputPassword = userPassword;
+    userObject.InputRemember = userRemember;
+
+    var userJson = JSON.stringify(userObject);
+
+    console.log(userJson);
+
+    $.ajax({
+        type: 'POST',
+        async: true,
+        url: '/Identity/Account/Login?handler=AjaxLogin',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN",
+                $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        data: userJson,
+        contentType: "application/json",
+        success: function (response) {
+            if (response === "Login Success!") {
+                console.log("Success!");
+                location.href = window.location.pathname;
+            }
+            if (response === "Invalid Credentials") {
+                $('#loginErrorMessage').html = "* Invalid credentials";
+            }
+        }
+    });
+});
