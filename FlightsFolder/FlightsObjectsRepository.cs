3e863a97c66013cf0416c107c7c781da37190c45
Sten-Mark Laur <sten@sml.ee>
2018-11-07 11:18:24 +0200

    Flight search backend update.

--- FlightsFolder/FlightsObjectsRepository.cs
+++ FlightsFolder/FlightsObjectsRepository.cs
@@ -4,13 +4,18 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace Vancouver.FlightsFolder
 {
     public class FlightsObjectsRepository : IFlightsObjectsRepository
     {
-
+        private IConfiguration _config;
+        public FlightsObjectsRepository(IConfiguration config)
+        {
+            _config = config;
+        }
         private static IList<FlightObject> flights { get; } = new List<FlightObject>();
 
         public async Task<FlightObject> GetObject(string id)
@@ -20,22 +25,38 @@ namespace Vancouver.FlightsFolder
 
         public async Task<IEnumerable<FlightObject>> GetObjectsList()
         {
-            var l = await jsonToObjectsList();
+            var l = await JsonToObjectsList("TLL", "SEA", "2018-11-10", "2018-11-25", "1", "ECONOMY", "EUR");
 
             flights.Clear();
             foreach (var i in l)
             {
                 flights.Add(i);
+
             }
 
             return new FlightObjectsList(l);
         }
 
-        private async Task<IEnumerable<FlightObject>> jsonToObjectsList()
+        public async Task<IEnumerable<FlightObject>> JsonToObjectsList(
+            string origin,
+            string destination,
+            string outboundDate,
+            string inboundDate,
+            string amountOfPassengers,
+            string travelClass,
+            string currency
+            )
         {
+            var requestUrl = @"https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" +
+                             _config["FlightSearchAPI:Key"] + "&origin=" + origin + "&destination=" +
+                             destination + "&departure_date=" + outboundDate + "&return_date=" + inboundDate +
+                             "&adults=" + amountOfPassengers + "&currency=" + currency + "&travel_class=" + travelClass;
             var request =
-                WebRequest.Create(
-                    @"https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=yE3GQEKE2YTzCFws0fdETDCc303eHVST");
+                WebRequest.Create(requestUrl);
+                    //@"https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" +
+                    //_config["FlightSearchAPI:Key"] + "&origin=" + origin + "&destination=" +
+                    //destination + "&departure_date=" + outboundDate + "&return_date=" + inboundDate +
+                    //"&adults=" + amountOfPassengers + "&currency=" + currency + "&travel_class=" + travelClass);
             var response = await request.GetResponseAsync().ConfigureAwait(false);
 
             var reader = new StreamReader(response.GetResponseStream());
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Vancouver.Databases;
 using Vancouver.FlightsFolder;
@@ -19,18 +21,81 @@ namespace Vancouver.Pages
         public UserManager<ApplicationUser> _userManager;
         public SignInManager<ApplicationUser> _signInManager;
         private readonly IFlightsObjectsRepository flightsRepository;
+        public FlightInputModel FlightInput { get; set; }
+        private IConfiguration _config;
+        
 
-        public IndexModel(VancouverDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IFlightsObjectsRepository fr)
+        public class FlightInputModel
+        {
+            public string OutboundAirport { get; set; }
+            public string InboundAirport { get; set; }
+            public string OutboundDate { get; set; }
+            public string InboundDate { get; set; }
+            public string AmountOfPassengers { get; set; }
+            public string TravelClass { get; set; }
+            public string Currency { get; set; }
+            public string FareOption { get; set; }
+        }
+
+        public IndexModel(VancouverDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration config)
         {
             _context = context;
             _userManager = userManager;
             _signInManager = signInManager;
-            flightsRepository = fr;
-            var l = flightsRepository.GetObjectsList();
+            _config = config;
         }
 
         [BindProperty]
         public Ticket Ticket { get; set; }
+
+        [BindProperty]
+        public FlightsResponse ResponseFlight { get; set; }
+        public FlightsResponse.RootObject rootFlight { get; set; }
+
+        public void OnPost()
+        {
+            
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<FlightsResponse.RootObject> GetResponse()
+        {
+            rootFlight = await JsonToObjectsList("TLL", "SEA", "2018-11-10", "2018-11-25", "1", "ECONOMY", "EUR");
+            return rootFlight;
+        }
+
+        public async Task<FlightsResponse.RootObject> JsonToObjectsList(
+            string origin,
+            string destination,
+            string outboundDate,
+            string inboundDate,
+            string amountOfPassengers,
+            string travelClass,
+            string currency
+        )
+        {
+            var requestUrl = @"https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" +
+                             _config["FlightSearchAPI:Key"] + "&origin=" + origin + "&destination=" +
+                             destination + "&departure_date=" + outboundDate + "&return_date=" + inboundDate +
+                             "&adults=" + amountOfPassengers + "&currency=" + currency + "&travel_class=" + travelClass;
+            var request =
+                WebRequest.Create(requestUrl);
+            //@"https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" +
+            //_config["FlightSearchAPI:Key"] + "&origin=" + origin + "&destination=" +
+            //destination + "&departure_date=" + outboundDate + "&return_date=" + inboundDate +
+            //"&adults=" + amountOfPassengers + "&currency=" + currency + "&travel_class=" + travelClass);
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var data = await reader.ReadToEndAsync();
+
+            return JsonConvert.DeserializeObject<FlightsResponse.RootObject>(data);
+        }
+
 
         public ActionResult OnPostGetTicket()
         {
@@ -90,10 +155,9 @@ namespace Vancouver.Pages
             }
         }
 
-        public void OnGet()
-        {
-            
-        }
+        
     }
+
+    
 }
 
