d0b0d834dc8952d97a6fe1ded0905928e1ed365f
Hanna-Liisa Vilbiks <havilb@ttu.ee>
2018-11-20 15:48:07 +0200

    Added the Archetype cs file, next step is creating the tests

--- CustomerFolder/Customer.cs
+++ CustomerFolder/Customer.cs
@@ -1,18 +1,46 @@
 ﻿using System;
 using System.ComponentModel;
+using Open.Core;
 using Vancouver.Models;
 
 namespace Vancouver.CustomerFolder
 {
-    public class Customer
+    public class Customer: Archetype
     {
-        public string CustomerId { get; set; }
+        private string customerId;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private DateTime validFrom = DateTime.MinValue;
+        private DateTime validTo = DateTime.MaxValue;
+
+
+        public string CustomerId {
+            get => getString(ref customerId);
+            set => customerId = value;
+        }
         [DisplayName("First Name")]
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get => getString(ref firstName);
+            set => firstName = value;
+        }
         [DisplayName("Last Name")]
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string LastName
+        {
+            get => getString(ref lastName);
+            set => lastName = value;
+        }
+        public string Email
+        {
+            get => getString(ref email);
+            set => email = value;
+        }
+        public DateTime DateOfBirth
+        {
+            get => getMinValue(ref validFrom, ref validTo);
+            set => setValue(ref validFrom, value);
+        }
 
         //public Address Address { get; set; }
         //public Contact Contact { get; set; }
