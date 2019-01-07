a8fdf7d545478c917819555cddb12100fbe6f9d3
dotSML <42400774+dotSML@users.noreply.github.com>
2019-01-07 14:51:50 +0200

    Front end fixes, code cleanup.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -165,8 +165,8 @@ namespace Vancouver.Areas.Identity.Pages.Account
                     values: new { userId = user.Id, code = emailToken },
                     protocol: Request.Scheme);
 
-                await _emailService.SendEmailAsync(Input.Email, "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                //await _emailService.SendEmailAsync(Input.Email, "Confirm your email",
+                //$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
 
 
